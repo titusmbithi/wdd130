4185e29da2f0fce575d34ef11dc72c07c9256ac6
titusmbithi <122625521+titusmbithi@users.noreply.github.com>
2023-02-11 21:26:57 +0300

    added

--- wwr/sentences.py
+++ wwr/sentences.py
@@ -1,41 +1,39 @@
-def get_determiner(quantity):
-    """Return a randomly chosen determiner. A determiner is
-    a word like "the", "a", "one", "some", "many".
-    If quantity is 1, this function will return either "a",
-    "one", or "the". Otherwise this function will return
-    either "some", "many", or "the".
+def get_preposition(quantity,random):
+    """Return a randomly chosen preposition
+    from this list of prepositions:
+        "about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"
 
-    Parameter
-        quantity: an integer.
-            If quantity is 1, this function will return a
-            determiner for a single noun. Otherwise this
-            function will return a determiner for a plural
-            noun.
-    Return: a randomly chosen determiner.
+    Return: a randomly chosen preposition.
     """
+
+
+
     if quantity == 1:
-        words = ["a", "one", "the"]
+        words = ["about", "across", "into"]
     else:
-        words = ["some", "many", "the"]
+        words = ["from", "with", "on"]
 
     # Randomly choose and return a determiner.
     word = random.choice(words)
     return word
 
 
-def get_noun(quantity):
-    """Return a randomly chosen noun.
-    If quantity is 1, this function will
-    return one of these ten single nouns:
-        "bird", "boy", "car", "cat", "child",
-        "dog", "girl", "man", "rabbit", "woman"
-    Otherwise, this function will return one of
-    these ten plural nouns:
-        "birds", "boys", "cars", "cats", "children",
-        "dogs", "girls", "men", "rabbits", "women"
+def get_prepositional_phrase(quantity):
+    """Build and return a prepositional phrase composed
+    of three words: a preposition, a determiner, and a
+    noun by calling the get_preposition, get_determiner,
+    and get_noun functions.
 
     Parameter
-        quantity: an integer that determines if
-            the returned noun is single or plural.
-    Return: a randomly chosen noun.
+        quantity: an integer that determines if the
+            determiner and noun in the prepositional
+            phrase returned from this function should
+            be single or pluaral.
+    Return: a prepositional phrase.
     """
+
